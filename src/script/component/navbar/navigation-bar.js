bd2572b0f5b73b7eca597971d3a9ea51bc0a930b
muhamadilyas17 <lyas170100@gmail.com>
2024-06-09 11:51:23 +0700

    Upload and update Navbar

--- src/script/component/navbar/navigation-bar.js
+++ src/script/component/navbar/navigation-bar.js
@@ -1,5 +1,34 @@
 class NavigationBar extends HTMLElement{
-    
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="#">Portofolio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#skripsi">Skripsi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#project">Project</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        `;        
+        
+    }
 }
 
 customElements.define('navigation-bar', NavigationBar);
