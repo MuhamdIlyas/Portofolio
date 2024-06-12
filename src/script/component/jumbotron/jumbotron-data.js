266f1e00efbb0f7f4152984a7906284bf28c694f
muhamadilyas17 <lyas170100@gmail.com>
2024-06-12 21:47:47 +0700

    Update Asset

--- src/script/component/jumbotron/jumbotron-data.js
+++ src/script/component/jumbotron/jumbotron-data.js
@@ -2,7 +2,7 @@ const data = [
 	{
 		title: 'My Motivation',
 		description: 'Jangan Pernah Menyerah Jika Kamu Masih Ingin Mencoba. Jangan Biarkan Penyesalan Datang, Karena Kamu Selangkah Lagi Untuk Menang,.',
-		gif: 'https://raw.githubusercontent.com/MuhamdIlyas/Portofolio/master/assets/Animation-1718202612396.json',
+		gif: 'https://raw.githubusercontent.com/MuhamdIlyas/Portofolio/master/assets/Animation1718202612396.json',
 		alt: 'Image Portofolio'
 	},
 ];
