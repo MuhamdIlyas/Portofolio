43655e434f44bc0427a5956dd9134a2d959d1ac5
muhamadilyas17 <lyas170100@gmail.com>
2024-06-12 18:42:39 +0700

    Create Jumbotron

---
+++ src/script/component/jumbotron/jumbotron-data.js
@@ -0,0 +1,11 @@
+const data = [
+	{
+		title: 'My Motivation',
+		description: 'Jangan Pernah Menyerah Jika Kamu Masih Ingin Mencoba. Jangan Biarkan Penyesalan Datang, Karena Kamu Selangkah Lagi Untuk Menang,.',
+		image: 'https://images.unsplash.com/photo-1421789665209-c9b2a435e3dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
+		alt: 'Image Portofolio'
+	},
+];
+
+export default data;
+
